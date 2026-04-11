44c7ba0a5db2b0757fed449857e2e4365fb515fa
Giovanni Blas Heredia <gblas-he@c3r7s6.42madrid.com>
2026-04-11 19:57:06 +0200

    python2

---
+++ python00/ex3/ft_harvest_total.py
@@ -0,0 +1,5 @@
+def ft_harvest_total() -> None:
+    d1 = int(input("Day 1 harvest: "))
+    d2 = int(input("Day 1 harvest: "))
+    d3 = int(input("Day 1 harvest: "))
+    print("Total harvest:", d1 + d2 + d3)

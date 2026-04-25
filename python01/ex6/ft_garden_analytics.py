52fb31b7e073047dbf95c4153dcf16068d095d76
Giovanni Blas Heredia <gblas-he@c3r5s2.42madrid.com>
2026-04-25 19:30:40 +0200

    ex6v1

--- python01/ex6/ft_garden_analytics.py
+++ python01/ex6/ft_garden_analytics.py
@@ -13,8 +13,10 @@ class Plant:
             self._ages = 0
         else:
             self._ages = ages
+        self.stats = self.Stats()
 
     def show(self) -> str:
+        self.stats.show += 1
         return (f"{self.name}: {round(self._height, 1)}cm, "
                 f"{self._ages} days old")
 
@@ -41,25 +43,38 @@ class Plant:
             print(f"Age updated: {ages} days")
 
     def grow(self) -> None:
+        self.stats.grow += 1
         self._height += 2.1
 
     def age(self) -> None:
+        self.stats.age += 1
         self._ages += 1
-        
+
     @staticmethod
     def more_year(year: int):
         print(f"Is {year} days more than a year? -> {year > 365}")
- 
+
     @classmethod
     def anonima(cls):
         return cls("Unknown plant", 0, 0)
+
+    class Stats:
+        def __init__(self):
+            self.grow = 0
+            self.age = 0
+            self.show = 0
+
+        def show_stats(self) -> None:
+            return (f"Stats: {self.grow} grow, {self.age} age, "
+                    f"{self.show} show")
+
 
 class Flower(Plant):
     def __init__(self, name: str, height: int, ages: int, color: str):
         super().__init__(name, height, ages)
         self.color = color
         self.is_bloomed = False
-    
+
     def bloom(self) -> None:
         self.is_bloomed = True
 
@@ -71,18 +86,30 @@ class Flower(Plant):
         else:
             print(f"{self.name} has not bloomed yet")
 
+
 class Tree(Plant):
     def __init__(self, name, height, age, trunk_diameter):
         super().__init__(name, height, age)
         self.trunk_diameter = trunk_diameter
 
     def produce_shade(self):
+        self.stats.shade += 1
         print(f"Tree {self.name} now produces a shade of "
               f"{self._height}cm long and {self.trunk_diameter}cm wide.")
 
     def show(self):
         print(super().show())
         print(f"Trunk diameter: {self.trunk_diameter}cm")
+
+    class Stats(Plant.Stats):
+        def __init__(self):
+            super().__init__()
+            self.shade = 0
+
+        def show_stats(self) -> None:
+            print(super().show_stats())
+            return (f"{self.shade} shade")
+
 
 class Vegetable(Plant):
     def __init__(self, name, height, age, harvest_season):
@@ -103,12 +130,24 @@ class Vegetable(Plant):
         print(f"Harvest season: {self.harvest_season}")
         print(f"Nutritional value: {self.nutritional_value}")
 
+
+class Seed(Flower):
+    def __init__(self, name: str, height: int, ages: int, color: str,
+                 seeds: int):
+        super().__init__(name, height, ages, color)
+        self.seeds = seeds
+
+    def show(self):
+        print(super().show())
+        print(f"Seeds: {self.seeds}")
+
+
 def main():
     print("=== Garden statistics ===")
     print("=== Check year-old")
     print("Is   days more than a year? -> ")
     print("Is   days more than a year? -> ")
-    
+
     print("=== Flower")
     rose = Flower("rose", 15, 10, "red")
     rose.show()

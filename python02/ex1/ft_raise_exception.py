18fd3e0567b1966a2444d0d39b8042b2a4f00450
Giovanni Blas Heredia <gblas-he@c3r13s4.42madrid.com>
2026-05-01 18:02:51 +0200

    p2v3

--- python02/ex1/ft_raise_exception.py
+++ python02/ex1/ft_raise_exception.py
@@ -2,33 +2,28 @@
 
 def input_temperature(tem_str: str) -> int:
     tem_num = int(tem_str)
-    if (tem_num <= 0):
+    if (tem_num < 0):
         raise Exception(f"Caught input_temperature error: {tem_num}°C "
-                        f"is too cold for plants (min {tem_num}°C)")
-    elif (tem_num >= 40):
+                        f"is too cold for plants (min 0°C)")
+    elif (tem_num > 40):
         raise Exception(f"Caught input_temperature error: {tem_num}°C "
-                        f"is too hot for plants (min {tem_num}°C)")
-
-
-
-    if (tem_num >= 0 and tem_num <= 40):
+                        f"is too hot for plants (max 40°C)")
+    else:
         return tem_num
-    raise Exception(f"Caught input_temperature error: {tem_num}°C is too cold "
-                    f"for plants (min {tem_num}°C)")
 
 
 def test_temperature() -> None:
-    test_values = ['25', 'abc', '100', '50']
+    test_values = ['25', 'abc', '100', '-50']
     for value in test_values:
+        print(f"Input data is '{value}'")
         try:
             tem = input_temperature(value)
-            print(f"Input data is '{value}'")
             print(f"Temperature is now {tem}ºC")
         except ValueError as v:
-            print(f"Input data is '{value}'")
             print(f"Caught input_temperature error: {v}")
         except Exception as e:
             print(f"{e}")
+        print()
 
 
 def main() -> None:

3432892d58485b3a1cf9dbd0cba9ca5aef897115
gblas-he <gblas-he@student.42madrid.com>
2026-07-23 14:26:16 +0200

    py03v5

--- python03/ex1/ft_score_analytics.py
+++ python03/ex1/ft_score_analytics.py
@@ -8,7 +8,7 @@ def main() -> None:
         scores = []
         for i in sys.argv[1:]:   # bucle convertir los argumento en int
             try:
-                scores.append(int(i))
+                scores.append(int(i)) # Añadir un elemento a una lista
                 # int(i) son cada argumento ya que sys.argv[i] es una lista
             except ValueError:
                 print(f"Invalid parameter: '{i}'")
--- python03/ex2/ft_coordinate_system.py
+++ python03/ex2/ft_coordinate_system.py
@@ -2,10 +2,15 @@
 
 def main() -> None:
     print("=== Player Score Analytics ===")
-    coordinate = input("Enter new coordinates as float in format 'x, y, z': ") 
-    coordinate = coordinate.split(",")
-    coordinate = float(coordinate)
-    print(coordinate)
+    coordinate = input("Enter new coordinates as float in format 'x, y, z': ") # Obtemos un string
+    coordinate = coordinate.split(",") # el string se vuelve tipo list, con elementos string, con split
+    float_coordinate = []
+    for i in coordinate: # float() no sabe convertir una lista es necesario un bucle
+        float_coordinate.append(float(i)) # Convertimos cada elemento en float y lo introducimos en la lista
+    tupla_coordinate = tuple(float_coordinate)
+    print(f"Got a first tuple: {tupla_coordinate}")
+    print(f"It includes: X={tupla_coordinate[0]}, Y={tupla_coordinate[1]}, Z={tupla_coordinate[2]}")
+    # print(f"Distance to center: {tupla_coordinate}")
 
 
 if __name__ == "__main__":

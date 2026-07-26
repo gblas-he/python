28be3787cdff7cde977c58bbdc5e4a950f60cf97
gblas-he <gblas-he@student.42madrid.com>
2026-07-26 16:33:04 +0200

    py3v7

--- python03/ex2/ft_coordinate_system.py
+++ python03/ex2/ft_coordinate_system.py
@@ -2,64 +2,46 @@
 import math
 
 
-def get_player_pos() -> tuple(float, float, float):
-    c = input("Enter new coordinates as float in format "
-               "'x, y, z': ")  # Obtemos un string
-    c = c.split(",")  # el string se vuelve tipo list,
-    # con elementos string, con split
-    float_c = []
-    for i in c:  # float() no sabe convertir una lista es
-        # necesario un bucle
-        float_c.append(float(i))  # Convertimos cada elemento en
-        # float y lo introducimos en la lista
-    tupla_c = tuple(float_c)
-    print(f"Got a first tuple: {tupla_c}")
-    print(f"It includes: X={tupla_c[0]}, Y={tupla_c[1]}, "
-          f" Z={tupla_c[2]}")
-    center = math.sqrt((tupla_c[0]**2) + (tupla_c[1]**2)
-                       + (tupla_c[2]**2))
-    print(f"Distance to center: {center}")
+def get_player_pos() -> tuple[float, float, float]:
+    while True:
+        raw = input("Enter new coordinates as float in format "
+                    "'x, y, z': ")  # Obtemos un string
+        c = raw.split(",")
+        # el string se vuelve tipo list, con elementos string, con split
+        if len(c) != 3:
+            print("Invalid syntax")
+            continue
+        error = False
+        float_c = []
+        for i in c:
+            # float() no sabe convertir una lista es necesario un bucle
+            try:
+                float_c.append(float(i))
+            # Convertimos cada elemento en float y lo introducimos en la lista
+            except ValueError as e:
+                print(f"Error on parameter '{i}': {e}")
+                error = True
+        if error:
+            continue
+        return (float_c[0], float_c[1], float_c[2])
 
 
 def main() -> None:
-    print("=== Game Coordinate System ===")
-    c1 = input("Enter new coordinates as float in format "
-               "'x, y, z': ")  # Obtemos un string
-    c1 = c1.split(",")  # el string se vuelve tipo list,
-    # con elementos string, con split
-    float_c1 = []
-    for i in c1:  # float() no sabe convertir una lista es
-        # necesario un bucle
-        float_c1.append(float(i))  # Convertimos cada elemento en
-        # float y lo introducimos en la lista
-    tupla_c1 = tuple(float_c1)
+    print("=== Game Coordinate System ===\n")
+    print("Get a first set of coordinates")
+    tupla_c1 = get_player_pos()
     print(f"Got a first tuple: {tupla_c1}")
     print(f"It includes: X={tupla_c1[0]}, Y={tupla_c1[1]}, "
-          f" Z={tupla_c1[2]}")
+          f"Z={tupla_c1[2]}")
     center = math.sqrt((tupla_c1[0]**2) + (tupla_c1[1]**2)
                        + (tupla_c1[2]**2))
-    print(f"Distance to center: {center}")
-
-    c2 = input("Enter new coordinates as float in format "
-               "'x, y, z': ")  # Obtemos un string
-    c2 = c2.split(",")  # el string se vuelve tipo list,
-    # con elementos string, con split
-    float_c2 = []
-    for i in c2:  # float() no sabe convertir una lista es
-        # necesario un bucle
-        float_c2.append(float(i))  # Convertimos cada elemento en
-        # float y lo introducimos en la lista
-    tupla_c2 = tuple(float_c2)
-    print(f"Got a second tuple: {tupla_c2}")
-    print(f"It includes: X={tupla_c2[0]}, Y={tupla_c2[1]}, "
-          f" Z={tupla_c2[2]}")
-    center = math.sqrt((tupla_c2[0]**2) + (tupla_c2[1]**2)
-                       + (tupla_c2[2]**2))
-    print(f"Distance to center: {center}")
-    distance = math.sqrt((tupla_c2[0] - tupla_c1[0])**2
-                         + (tupla_c2[1] - tupla_c1[1])**2
-                         + (tupla_c2[2] - tupla_c1[2])**2)
-    print(f"Distance between the 2 sets of coordinates: {distance}")
+    print(f"Distance to center: {round(center, 4)}\n")
+    print("Get a second set of coordinates")
+    tupla_c2 = get_player_pos()
+    distance = math.sqrt((tupla_c1[0] - tupla_c2[0])**2
+                         + (tupla_c1[1] - tupla_c2[1])**2
+                         + (tupla_c1[2] - tupla_c2[2])**2)
+    print(f"Distance between the 2 sets of coordinates: {round(distance, 4)}")
 
 
 if __name__ == "__main__":
--- python03/ex2/prueba.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3.10
-
-texto = "10,20,30"
-
-print(texto)
-print(texto.split(","))
-print(texto)

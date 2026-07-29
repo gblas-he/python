f4db9567d9a3a0dcbdbdc92e22bf26235af52ad5
gblas-he <gblas-he@student.42madrid.com>
2026-07-29 18:59:57 +0200

    py3v11

--- python03/ex4/ft_inventory_system.py
+++ python03/ex4/ft_inventory_system.py
@@ -2,23 +2,23 @@
 import sys
 
 
-def gen_player_achievements(ach: list) -> set:
-
-
 def main() -> None:
-    print("=== Inventory System Analysis ===\n")
-    if len(sys.argv) > 1:
-        for i in sys.argv[1:]:   # bucle convertir los argumento en int
-            try:
-                partes = i.split(":")
-                if len(partes) != 2:
-                    print("Error - ivalid parameter '{i}'")
-                    continue
-                else:
-                    inventory.append(int(i))  # Añadir un elemento a una lista
-                # int(i) son cada argumento ya que sys.argv[i] es una lista
-            except ValueError:
-                print(f"Invalid parameter: '{i}'")
+    print("=== Inventory System Analysis ===")
+    inventory = {}
+    for i in sys.argv[1:]:   # bucle convertir los argumento en diccionario
+        try:
+            partes = i.split(":")
+            if len(partes) != 2:
+                print(f"Error - invalid parameter '{i}'")
+                continue
+            elif partes[0] in inventory:
+                    print(f"Redundant item '{partes[0]}' - discarding")
+            else:
+                inventory[partes[0]] = int(partes[1])  # Añadir un elemento a un diccionario
+        except ValueError as e:
+            print(f"Quantity error for '{partes[0]}': {e}")
+    print(f"Got inventory: {inventory}")
+         
 
 
 if __name__ == "__main__":

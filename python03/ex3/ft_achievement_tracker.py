3d070f5d5039e48e3238ae2efea263daaa03abe3
gblas-he <gblas-he@student.42madrid.com>
2026-07-29 11:32:59 +0200

    p3v9

--- python03/ex3/ft_achievement_tracker.py
+++ python03/ex3/ft_achievement_tracker.py
@@ -4,22 +4,36 @@ import random
 
 def gen_player_achievements(ach: list) -> set:
     num_ach = random.randint(1, len(ach))
-    achievements = random.sample(ach, k = num_ach)
+    achievements = random.sample(ach, k=num_ach)
     return set(achievements)
-
 
 
 def main() -> None:
     print("=== Achievement Tracker System ===\n")
-    ACHIEVEMENT_POOL = ['Crafting Genius', 'Strategist', 'World Savior', 'Speed Runner', 'Survivor',
-                        'Master Explorer', 'Treasure Hunter', 'Unstoppable', 'First Steps', 
-                        'Collector Supreme', 'Untouchable', 'Sharp Mind', 'Boss Slayer']
-    print(f"Player Alice: {gen_player_achievements(ACHIEVEMENT_POOL)}")
-    print(f"Player Bob: {gen_player_achievements(ACHIEVEMENT_POOL)}")
-    print(f"Player Charlie: {gen_player_achievements(ACHIEVEMENT_POOL)}")
-    print(f"Player Dylan: {gen_player_achievements(ACHIEVEMENT_POOL)}")
-    print(f"All distinct achievements: ")
-    print(f"Common achievements:: ")
+    ACHIEVEMENT_POOL = ['Crafting Genius', 'Strategist', 'World Savior',
+                        'Speed Runner', 'Survivor', 'Master Explorer',
+                        'Treasure Hunter', 'Unstoppable', 'First Steps',
+                        'Collector Supreme', 'Untouchable', 'Sharp Mind',
+                        'Boss Slayer']
+    p1 = gen_player_achievements(ACHIEVEMENT_POOL)
+    p2 = gen_player_achievements(ACHIEVEMENT_POOL)
+    p3 = gen_player_achievements(ACHIEVEMENT_POOL)
+    p4 = gen_player_achievements(ACHIEVEMENT_POOL)
+    ach_set = set(ACHIEVEMENT_POOL)
+    print(f"Player Alice: {p1}")
+    print(f"Player Bob: {p2}")
+    print(f"Player Charlie: {p3}")
+    print(f"Player Dylan: {p4}\n")
+    print(f"All distinct achievements: {p1.union(p2, p3, p4)}\n")
+    print(f"Common achievements: {p1.intersection(p2, p3, p4)}\n")
+    print(f"Only Alice has: {p1.difference(p2, p3, p4)}")
+    print(f"Only Bob has: {p2.difference(p1, p3, p4)}")
+    print(f"Only Charlie has: {p3.difference(p1, p2, p4)}")
+    print(f"Only Dylan has: {p4.difference(p1, p2, p3)}\n")
+    print(f"Alice is missing: {ach_set.difference(p1)}")
+    print(f"Bob is missing: {ach_set.difference(p2)}")
+    print(f"Charlie is missing: {ach_set.difference(p3)}")
+    print(f"Dylan is missing: {ach_set.difference(p4)}")
 
 
 if __name__ == "__main__":
---
+++ python03/ex4/ft_inventory_system.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3.10
+
+def gen_player_achievements(ach: list) -> set:
+
+
+def main() -> None:
+    print("=== Achievement Tracker System ===\n")
+
+
+if __name__ == "__main__":
+    main()
